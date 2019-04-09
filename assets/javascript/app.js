73f59ffb3ef3daf812cfbedcf421edb5f78c6efa
Jaime Tyler <jaimetyler88@gmail.com>
2019-04-09 18:10:53 -0400

    took off the geolocation code to test branch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,3 @@
-
-var x = document.getElementById("demo");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
 $(document).ready(function () {
   $("#artist-review").hide();
   $(".wrap").hide();
