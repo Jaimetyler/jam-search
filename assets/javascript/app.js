c71c58486930ee5510b3050b1d203109fbde43f9
Jaime Tyler <jaimetyler88@gmail.com>
2019-04-10 23:27:11 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,101 +1,109 @@
-$(document).ready(function(){
-    $("#artist-review").hide();
+$(document).ready(function () {
+  $("#artist-review").hide();
+  $(".wrap").hide();
+
+  $("#submit").on("click", function () {
+
     $(".wrap").hide();
-  
-    $("#submit").on("click", function () {
-  
-        $(".wrap").hide();
-        $(".img").show();
-        //$(code to send to firebase).????();
-        $("#textarea").val('');
-        $("#review").show();
-    })
-    $("#close").on("click", function () {
-  
-        $(".wrap").hide();
-        $(".img").show();
-        $("#review").show();
-  
-    })
-  
-    $(".img").on("click", function () {
-  
-        $(".wrap").show();
-        $(".img").hide();
-        $("#review").hide();
-  
-    })
-  
+    $(".img").show();
+    //$(code to send to firebase).????();
+    $("#textarea").val('');
+    $("#review").show();
+  })
+  $("#close").on("click", function () {
 
- // Initialize Firebase  
-var config = {
-  apiKey: "AIzaSyBFpSgNqrH_XGmnK5tJgp7s-qPlDqe2vUM",
-  authDomain: "project-x-60d84.firebaseapp.com",
-  databaseURL: "https://project-x-60d84.firebaseio.com",
-  projectId: "project-x-60d84",
-  storageBucket: "project-x-60d84.appspot.com",
-  messagingSenderId: "905166809578"
-};  
-firebase.initializeApp(config);
+    $(".wrap").hide();
+    $(".img").show();
+    $("#review").show();
 
-const reviewData = firebase.database(artist);
+  })
 
-//var key = reviewData.key;
-//var reviewArtist = firebase.database().ref().key();
+  $(".img").on("click", function () {
 
-//creating variables that will be stored
-var artistSearched;
-var numberOfSearches = 0;
+    $(".wrap").show();
+    $(".img").hide();
+    $("#review").hide();
+
+  })
 
 
-$("#submit").on("click", function(){
-  event.preventDefault();
+  // Initialize Firebase  
+  var config = {
+    apiKey: "AIzaSyBFpSgNqrH_XGmnK5tJgp7s-qPlDqe2vUM",
+    authDomain: "project-x-60d84.firebaseapp.com",
+    databaseURL: "https://project-x-60d84.firebaseio.com",
+    projectId: "project-x-60d84",
+    storageBucket: "project-x-60d84.appspot.com",
+    messagingSenderId: "905166809578"
+  };
+  firebase.initializeApp(config);
 
-  review = $("#artistReview").val().trim();
+  const reviewData = firebase.database(artist);
 
-  //how can i make this a parent in the data tree?????
-  artistSearched = $("#query").val().trim();
+  //var key = reviewData.key;
+  //var reviewArtist = firebase.database().ref().key();
+
+  //creating variables that will be stored
+  var artistSearched;
+  var numberOfSearches = 0;
+
+  function writeReview(artistSearched) {
+      reviewData.ref(artistSearched).set({
+      artistSearched: artistSearched,
+      review: review,
+      dataAdded: firebase.database.ServerValue.TIMESTAMP
+
+  });
+  }
+
+  $("#submit").on("click", function () {
+    event.preventDefault();
+
+    review = $("#artistReview").val().trim();
+
+    //how can i make this a parent in the data tree?????
+    artistSearched = $("#query").val().trim();
+
+    writeReview();
+    //reviewData.ref().push({
+    // artistSearched: artistSearched,
+    // numberOfSearches: numberOfSearches,
+    //review: review,
+    //dateAdded: firebase.database.ServerValue.TIMESTAMP
+    //});
+
+    //can i loop through this and append each review that finds the searched artist?
 
 
-  reviewData.ref().push({
-    artistSearched: artistSearched,
-   // numberOfSearches: numberOfSearches,
-   review: review,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
+    console.log(artistSearched);
+    console.log(review);
   });
 
- //can i loop through this and append each review that finds the searched artist?
 
+  //global variable
 
-console.log(artistSearched);
-console.log(review);
-});
+  var artist = $("#query").val().trim();
 
+  function searchBandEvents(artist) {
+    console.log(artist);
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=47972149c9ef95f0470de3a7f2d73af9&date=upcoming";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      //empty table 
+      $("#events-table").empty();
+      console.log(response);
+      for (i = 0; i < response.length; i++) {
 
-//global variable
+        var eventDate = response[i].datetime;
+        var venueName = response[i].venue.name;
+        var location = response[i].venue.city;
+        var ticketURL = response[i].offers[0].url;
 
-var artist = $("#query").val().trim();
+        var datePretty = moment(eventDate).format("MM/DD/YYYY");
 
-function searchBandEvents(artist) {
-  console.log(artist);
-  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=47972149c9ef95f0470de3a7f2d73af9&date=upcoming";
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    //empty table 
-    $("#events-table").empty();
-    console.log(response);
-    for (i = 0; i < response.length; i++) {
-
-    var eventDate = response[i].datetime;  
-    var venueName = response[i].venue.name;
-    var location = response[i].venue.city;
-    var ticketURL = response[i].offers[0].url;
-
-    var datePretty = moment(eventDate).format("MM/DD/YYYY");
-    
-    $("#events-table").append(`
+        $("#events-table").append(`
       <tr>
       <td> ${datePretty} </td>
       <td> ${venueName} </td>
@@ -105,13 +113,13 @@ function searchBandEvents(artist) {
       </tr>
 
 
-    `)  
-    }
-    
-  });  
+    `)
+      }
+
+    });
 
 
-}  
+  }
 
 
 
@@ -122,11 +130,11 @@ function searchBandEvents(artist) {
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
 
       // Printing the entire object to console
       console.log(response);
-      
+
 
       // Constructing HTML containing the artist information
       var artistName = $("<h1>").text(response.name);
@@ -139,11 +147,11 @@ function searchBandEvents(artist) {
       // Empty the contents of the artist-div, append the new artist content
       $("#artist-div").empty();
       $("#artist-div").append(artistURL, artistImage, trackerCount, upcomingEvents, goToArtist);
-    });  
-  }  
+    });
+  }
 
   // Event handler for user clicking the select-artist button
-  $("#select-artist").on("click", function(event) {
+  $("#select-artist").on("click", function (event) {
     // Preventing the button from trying to submit the form
     event.preventDefault();
     // Storing the artist name
@@ -153,92 +161,93 @@ function searchBandEvents(artist) {
     searchBandsInTown(inputArtist);
     searchBandEvents(inputArtist);
     search();
-    
-  });  
-  
 
-
-function search () {
-
-$('#results').html('');
-$('#buttons').html('');
-
-//Get For input
-q = $('#query').val();
-
-//run Get Request on API
-
-$.get(
-  "https://www.googleapis.com/youtube/v3/search", {
-      part: 'snippet, id',
-      q: q,
-      type:'video',
-      key: 'AIzaSyAuU2FUW6RgwQSKOnI0TQDGGkJxBG81ksA'},
-
-      function(data) {
-          var nextPageToken = data.nextPageToken;
-          var prevPageToken = data.prevPageToken;
-
-
-          //loging the data
-          console.log(data);
-
-          $.each(data.items, function(i, item){
-
-
-              //getting the output
-              var output = getOutput (item);
-
-              //Display Results
-
-              $('#results').append(output);
+  });
 
 
 
+  function search() {
 
-          });
-          
+    $('#results').html('');
+    $('#buttons').html('');
+
+    //Get For input
+    q = $('#query').val();
+
+    //run Get Request on API
+
+    $.get(
+      "https://www.googleapis.com/youtube/v3/search", {
+        part: 'snippet, id',
+        q: q,
+        type: 'video',
+        key: 'AIzaSyAuU2FUW6RgwQSKOnI0TQDGGkJxBG81ksA'
+      },
+
+      function (data) {
+        var nextPageToken = data.nextPageToken;
+        var prevPageToken = data.prevPageToken;
+
+
+        //loging the data
+        console.log(data);
+
+        $.each(data.items, function (i, item) {
+
+
+          //getting the output
+          var output = getOutput(item);
+
+          //Display Results
+
+          $('#results').append(output);
+
+
+
+
+        });
+
 
       }
-);
-};
+    );
+  };
 
 
-function getOutput (item){
+  function getOutput(item) {
 
-  var videoId = item.id.videoId;
+    var videoId = item.id.videoId;
 
-  var title = item.snippet.title;
-  var description = item.snippet.description;
-  var thumb = item.snippet.thumbnails.high.url;
-  var channelTitle = item.snippet.ChannelTitle;
-  var videoDate = item.snippet.publishedAt;
+    var title = item.snippet.title;
+    var description = item.snippet.description;
+    var thumb = item.snippet.thumbnails.high.url;
+    var channelTitle = item.snippet.ChannelTitle;
+    var videoDate = item.snippet.publishedAt;
 
-  // appends to the html
-  var output = '<li>' +
-  '<div class = "list-left">' +
-  '<img src="'+thumb+'">' +
-  '</div>' +
-  '<div class="list-right">' +
-  '<h3><a class = "iframe" href="http://www.youtube.com/watch/'+videoId+'"target="_blank">'+title+'</a></h3>' +
-  '<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
-  '<p>'+description+'</p>' +
-  '</div>'+
-  '</li>' +
-  '';
-
-
-
-
-
-  return output;
+    // appends to the html
+    var output = '<li>' +
+      '<div class = "list-left">' +
+      '<img src="' + thumb + '">' +
+      '</div>' +
+      '<div class="list-right">' +
+      '<h3><a class = "iframe" href="http://www.youtube.com/watch/' + videoId + '"target="_blank">' + title + '</a></h3>' +
+      '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + '</small>' +
+      '<p>' + description + '</p>' +
+      '</div>' +
+      '</li>' +
+      '';
 
 
 
 
 
+    return output;
 
-}
+
+
+
+
+
+  }
 
 
 });
